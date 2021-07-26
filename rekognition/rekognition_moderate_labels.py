17a6b157edfcbce27c63f97a36f0fe03572cc38d
jmstio <87942761+jmstio@users.noreply.github.com>
2021-07-26 19:08:11 +0100

    Add files via upload

---
+++ rekognition/rekognition_moderate_labels.py
@@ -0,0 +1,29 @@
+#Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#PDX-License-Identifier: MIT-0 (For details, see https://github.com/awsdocs/amazon-rekognition-developer-guide/blob/master/LICENSE-SAMPLECODE.)
+
+import boto3
+
+def moderate_image(photo, bucket):
+
+    client=boto3.client('rekognition')
+
+    response = client.detect_moderation_labels(Image={'S3Object':{'Bucket':bucket,'Name':photo}})
+
+    print('Detected labels for ' + photo)    
+    for label in response['ModerationLabels']:
+        print (label['Name'] + ' : ' + str(label['Confidence']))
+        print (label['ParentName'])
+    return len(response['ModerationLabels'])
+
+
+
+def main():
+    bucket='content-moderation-jt-london'
+    photo='man-woman-beach.jpg'
+    label_count=moderate_image(photo, bucket)
+    print("Labels detected: " + str(label_count))
+
+
+if __name__ == "__main__":
+    main()
+ 
