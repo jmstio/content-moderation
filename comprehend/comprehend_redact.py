7c5ec12922527cf4965f17cc6a0f3290df31ff01
Thompson <jjamt@147dda75a77c.ant.amazon.com>
2021-07-27 09:47:58 +0100

    fixed redact

---
+++ comprehend/comprehend_redact.py
@@ -0,0 +1,27 @@
+
+import boto3
+
+client = boto3.client('comprehend')
+
+response = client.start_pii_entities_detection_job(
+    InputDataConfig={
+        'S3Uri': 's3://content-moderation-jt-london/detect_sample.txt',
+        'InputFormat': 'ONE_DOC_PER_FILE'
+    },
+    OutputDataConfig={
+        'S3Uri': 's3://content-moderation-jt-london',
+        'KmsKeyId': 'arn:aws:kms:eu-west-2:858545927766:alias/content-moderation-s3-pii-redaction'
+    },
+    Mode='ONLY_REDACTION',
+    RedactionConfig={
+        'PiiEntityTypes': [
+            'ALL'
+        ],
+        'MaskMode': 'MASK',
+        'MaskCharacter': '*'
+    },
+    DataAccessRoleArn='arn:aws:iam::858545927766:role/content-moderation-for-s3',
+    JobName='content-moderation-redaction',
+    LanguageCode='en',
+    #ClientRequestToken='string'
+)
